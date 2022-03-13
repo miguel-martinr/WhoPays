dd7dc8e1c64b9503f5077b3e96a2e460e011754c
Miguel Martín <alu0101209777@ull.edu.es>
2022-03-13 21:23:25 +0000

    import fix

--- src/features/Payer/Payer.tsx
+++ src/features/Payer/Payer.tsx
@@ -1,9 +1,9 @@
-import { Col, Form, Row } from 'react-bootstrap';
+import { Col, Form } from 'react-bootstrap';
 import { PayerProps } from '../../types/PayerProps';
 import { setPayerBeingLinked, updatePayer } from '../state/whoPaysSlice';
 import { NameInput } from '../Utils/NameInput';
 import { LinkButton } from '../Utils/Buttons/LinkButton';
-import { useAppDispatch, useAppSelector, useSmallDisplay } from '../../app/hooks';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { ResponsiveRow } from '../Utils/ResponsiveRow';
 
 export interface PayerComponentProps {
@@ -30,8 +30,6 @@ export const Payer = ({ payer }: PayerComponentProps) => {
     if (payerBeingLinkedId === '') return dispatch(setPayerBeingLinked(payer.id));
     return dispatch(setPayerBeingLinked(''));
   }
-
-  const idDisplaySmall = useSmallDisplay();
 
   return (
 
